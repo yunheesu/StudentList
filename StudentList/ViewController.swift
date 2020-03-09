b27514ccebf315d28a4bb4627a7abe3564370d40
Heesu Yun <Yunheesu@icloud.com>
2020-03-09 16:35:46 -0400

    Initial Commit

---
+++ StudentList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentList
+//
+//  Created by Heesu Yun on 3/9/20.
+//  Copyright © 2020 Heesu Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
